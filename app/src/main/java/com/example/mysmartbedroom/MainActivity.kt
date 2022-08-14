49c6fff07dfcdf9484c7ba2fe5847c6090ef2134
Brendon Rodrigues <brendonrodrigues1999@gmail.com>
2022-08-14 21:07:49 +0100

    Android: implemeted side navigation

--- app/src/main/java/com/example/mysmartbedroom/MainActivity.kt
+++ app/src/main/java/com/example/mysmartbedroom/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.mysmartbedroom
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +18,11 @@ class MainActivity : AppCompatActivity() {
         if(auth.currentUser == null){
             startActivity(Intent(this,LoginActivity::class.java))
             finish()
+        }
+        val drawerlayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val menuBtn = findViewById<ImageView>(R.id.imageMenu)
+        menuBtn.setOnClickListener{
+            drawerlayout.openDrawer(GravityCompat.START)
         }
     }
 }
