4392ec297d14d3dd9c692a16558351d25baa7b39
Brendon Rodrigues <brendonrodrigues1999@gmail.com>
2022-08-15 16:22:46 +0100

    Android: Implemented Signout user functionality

--- app/src/main/java/com/example/mysmartbedroom/MainActivity.kt
+++ app/src/main/java/com/example/mysmartbedroom/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +25,23 @@ class MainActivity : AppCompatActivity() {
         val menuBtn = findViewById<ImageView>(R.id.imageMenu)
         menuBtn.setOnClickListener{
             drawerlayout.openDrawer(GravityCompat.START)
+        }
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        navigationView.setNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.menuProfile -> {
+
+                    true
+                }
+                R.id.menuLogout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    startActivity(Intent(applicationContext, LoginActivity::class.java))
+                    finish()
+                    Toast.makeText(this,"Logged Out..", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else-> false
+            }
         }
     }
 }
